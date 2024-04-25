bbce92514fdebb466791992dd2ea2cb18834aab5
rodeok <nationx22@gmail.com>
2024-04-25 18:43:08 +0100

    db

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS
 
 # Get the base URL from the environment variable
-BASE_URL = os.environ.get('BASE_URL', 'https://flaskdb-mt6w.onrender.com/')
+BASE_URL = os.environ.get('BASE_URL', 'https://flaskdb-mt6w.onrender.com')
+# BASE_URL = os.environ.get('BASE_URL', 'localhost:5000') 
 
 # In-memory data structure to store URL mappings
 url_mappings = {}
@@ -19,7 +20,9 @@ def generate_short_url():
     if short_url in url_mappings:
         return generate_short_url()
     return short_url
-
+@app.route("/")
+def home():
+    return ("hi")
 @app.route('/api/shorten', methods=['POST'])
 def shorten_url():
     long_url = request.json['longUrl']
